6a045825e19ea9cae261cd1eff9fcacdfec9a16c
Kerlos Magdy <kerlossony@yahoo.com>
2025-08-01 00:11:09 +0300

    Fix Background main

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -2,7 +2,7 @@
 export default function Background({ linesOnly, blueOnly, image }: { blueOnly?: boolean, linesOnly?: boolean, image?: string }) {
     return (
         <div className={`absolute w-[100%] rounded-[25px] max-h-[853px] ${linesOnly ? "z-[4]" : "-z-[10]"} h-full  overflow-hidden`}>
-            {!linesOnly && <img src={image || '/assets/home-01.jpg'} alt='background' className='object-cover h-full' />}
+            {!linesOnly && <img src={image || '/assets/home-01.jpg'} alt='background' className='object-cover h-full w-full' />}
             {!blueOnly &&
                 <img src={'/assets/lines.png'} alt='background' className={`${linesOnly && "opacity-40"} object-cover`} />
             }
