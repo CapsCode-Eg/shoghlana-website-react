3ccd6999f314767b8adbda43b1fd20bda95bdf01
Kerlos Magdy <kerlossony@yahoo.com>
2025-08-29 16:28:35 +0300

    fix search

--- src/components/tabsSearchHomePage/tabsSearchHomePage.tsx
+++ src/components/tabsSearchHomePage/tabsSearchHomePage.tsx
@@ -15,7 +15,7 @@ export default function TabsSearchHomePage({ setData }: any) {
     return (
         <div className='flex flex-row gap-4 mt-4 items-center w-full max-w-full overflow-x-auto'>
             <span onClick={() => handleChange(1)} className={`w-[100px] text-nowrap md:w-[101px] h-[24px] md:h-[37px] text-center text-[11px] md:text-[15px] px-1.5 md:px-3 flex items-center justify-center font-[600] cursor-pointer ${tab === 1 ? 'text-white bg-main rounded-md' : 'text-[#818181]'} `}>
-                Full Time
+                On Site
             </span>
             <span onClick={() => handleChange(2)} className={`w-[80px] text-nowrap md:w-[101px] h-[24px] md:h-[37px] text-center text-[11px] md:text-[15px] px-1.5 md:px-3 flex items-center justify-center font-[600] cursor-pointer ${tab === 2 ? 'text-white bg-main rounded-md' : 'text-[#818181]'} `}>
                 Hybird
